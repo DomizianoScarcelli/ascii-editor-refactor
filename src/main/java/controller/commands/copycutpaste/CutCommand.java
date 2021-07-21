c7deaa0480e1f404f4d368fba8559eec3441639f
Domiziano Scarcelli <scarcelli.d@outlook.com>
2021-07-21 12:14:19 +0200

    Implemented cut

--- src/main/java/controller/commands/copycutpaste/CutCommand.java
+++ src/main/java/controller/commands/copycutpaste/CutCommand.java
@@ -1,11 +1,25 @@
 package controller.commands.copycutpaste;
 
 import controller.commands.Command;
+import view.MainPanel;
+import view.RightClickMenu;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class CutCommand implements Command {
+    private MainPanel mainPanel = MainPanel.getInstance();
     @Override
     public void execute() {
-
+        System.out.println("Tagliato");
+        RightClickMenu.getInstance().setCopiedChars(mainPanel.selectedPoints);
+        mainPanel.selectedPoints = new ArrayList<>();
+        for (int[] point : RightClickMenu.getInstance().getCopiedChars()){
+            mainPanel.getAsciiPanel().setCursorX(point[0]);
+            mainPanel.getAsciiPanel().setCursorY(point[1]);
+            mainPanel.getAsciiPanel().write((char) 0, Color.BLACK, Color.BLACK);
+        }
+        mainPanel.getAsciiPanel().repaint();
     }
 
     @Override
